4070df2dc25da65e728de5b5a8baf13743921592
“Karen <karen.arrieta1991@gmail.com>
2023-06-30 11:32:57 -0500

    Actualizando proyecto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../src/bootstrap.min.css';
+import '../src/responsive.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
